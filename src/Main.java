ce8fa9e95722d1a15b9630b569214e63ad699e5a
PLNA88 <paulina.gigowska@gmail.com>
2022-01-16 00:57:43 +0100

    ćwiczenie 6

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,33 @@
+import java.util.Scanner;
 import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.util.*;
 
 public class Main {
 
+    public static void metodaPierwszeB(ArrayList<String> lista1)
+    {
+        System.out.println("\nWprowadzone nazwy zwierząt:");
+        for (var zwierze: lista1)
+        {
+            System.out.println(zwierze);
+        }
+        System.out.println();
+    }
+    public static void metodaPierwszeC(ArrayList<String> lista1)
+    {
+        System.out.println("\nWprowadzone nazwy zwierząt:");
+        for (var zwierze: lista1)
+        {
+            System.out.println(zwierze);
+        }
+        System.out.println();
+        System.out.println("Ilość wprowadzonych zwierząt: "+lista1.size());
+    }
+
     public static void main(String[] args) {
 
         /* ćw_6 Listy */
-
         /* zad.1 a) Napisać program który stworzy listę ArrayList<String> (lista tablicowa),
                 Proszę dane wprowadzać z klawiatury (można użyć pętli), lista ma mieć przynajmniej 5
                 elementów i elementami mają być nazwy zwierząt.
@@ -18,13 +38,41 @@ public class Main {
                 d) następnie proszę wykonać odwrotne sortowanie i znów użyć metody z punktu b) do wyświetlenia
                 elementów listy
           */
-
-
          /* zad.2 Napisać program który stworzy Set<Integer> i wprowadzamy 10 liczb z klawiatury
                 niech liczby się powtarzają, proszę posortować a następnie wyświetlić zawartość set-a w konsoli
                  a następnie przeiterować i wyświetlić forEach-em (jak wyżej)
          */
 
-    }
+        System.out.println("zad. 1");
+        ArrayList<String> lista1 = new ArrayList<>();
+        Scanner z = new Scanner(System.in);
+        for (int i = 0; i <6; ++i)
+        {
+            System.out.println("Wprowadź nazwę zwierzęcia:");
+            lista1.add(z.next());
+        }
 
+        metodaPierwszeB(lista1);
+
+        lista1.remove(lista1.size()-1);
+        lista1.remove(lista1.size()-1);
+        lista1.add("krowa");
+        lista1.add("baran");
+        lista1.add("kogut");
+        metodaPierwszeC(lista1);
+
+        lista1.sort(Collections.reverseOrder());
+        metodaPierwszeB(lista1);
+
+        System.out.println("\nzad. 2");
+        Set<Integer> set2 = new TreeSet<>();
+        Scanner s = new Scanner(System.in);
+        for (int i = 0; i < 10; i++)
+        {
+            System.out.println("Podaj element:");
+            set2.add(s.nextInt());
+        }
+        System.out.println("\nWprowadzone wartości: "+set2);
+        set2.forEach(System.out::println);
+    }
 }
